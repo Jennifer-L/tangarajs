b0016a783ba29d8fd67ac6848c433f12e8d231a2
Jennifer-L <jennifer.laroze@gmail.com>
2014-03-14 10:19:07 +0100

    Update TEssai.js

--- js/tangara/TEssai.js
+++ js/tangara/TEssai.js
@@ -1,5 +1,5 @@
 define(['jquery', 'split-pane','TCanvas', 'TEnvironment', 'TEditor', 'TLog'], function($, SplitPane, TCanvas, TEnvironment, TEditor, TLog, TEssai) {
-  function TFrame() {
+  function TDisplay() {
   	console.log("Hello world ! Ce message est un test");
   }
 }
